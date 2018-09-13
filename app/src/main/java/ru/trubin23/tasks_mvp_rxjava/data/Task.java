6f925afb1a446f1e7c78516cf43bb3bc10139bc5
Trubin Andrey <trubin23@gmail.com>
2018-09-13 23:54:15 +0500

    begin to implement data package

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/Task.java
@@ -0,0 +1,25 @@
+package ru.trubin23.tasks_mvp_rxjava.data;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public final class Task {
+
+    @NonNull
+    private final String mId;
+
+    @Nullable
+    private final String mTitle;
+
+    @Nullable
+    private final String mDescription;
+
+    private final boolean mCompleted;
+
+    public Task(@NonNull String id, @Nullable String title, @Nullable String description, boolean completed) {
+        mId = id;
+        mTitle = title;
+        mDescription = description;
+        mCompleted = completed;
+    }
+}
