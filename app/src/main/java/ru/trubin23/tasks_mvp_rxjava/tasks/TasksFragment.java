d08cc4ebae443bcbca799e28978d837a16911b75
Trubin Andrey <trubin23@gmail.com>
2018-09-14 23:32:19 +0500

    start to implement TasksFragment class

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/tasks/TasksFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/tasks/TasksFragment.java
@@ -1,13 +1,33 @@
 package ru.trubin23.tasks_mvp_rxjava.tasks;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 
-public class TasksFragment extends Fragment implements TasksContract.View{
+public class TasksFragment extends Fragment implements TasksContract.View {
 
     private TasksContract.Presenter mPresenter;
 
+    @NonNull
+    public static TasksFragment newInstance() {
+        return new TasksFragment();
+    }
+
     @Override
     public void setPresenter(TasksContract.Presenter presenter) {
-
+        mPresenter = presenter;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mPresenter.subscribe();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mPresenter.unsubscribe();
+    }
+
+
 }
