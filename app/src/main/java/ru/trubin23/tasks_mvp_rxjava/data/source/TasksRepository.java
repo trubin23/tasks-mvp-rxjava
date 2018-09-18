0feb2b4a5e5efadfd5ee1a63a7c0ab432064278e
Trubin Andrey <trubin23@gmail.com>
2018-09-18 22:58:56 +0500

    continue to implementing TasksRepository class

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/source/TasksRepository.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/source/TasksRepository.java
@@ -1,6 +1,7 @@
 package ru.trubin23.tasks_mvp_rxjava.data.source;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.common.base.Optional;
 
@@ -10,6 +11,31 @@ import io.reactivex.Flowable;
 import ru.trubin23.tasks_mvp_rxjava.data.Task;
 
 public class TasksRepository implements TasksDataSource {
+
+    @Nullable
+    private static TasksRepository INSTANCE = null;
+
+    private final TasksDataSource mTasksRemoteDataSource;
+    private final TasksDataSource mTasksLocalDataSource;
+    private final TasksDataSource mTasksCacheDataSource;
+
+    private TasksRepository(@NonNull TasksDataSource tasksRemoteDataSource,
+                            @NonNull TasksDataSource tasksLocalDataSource,
+                            @NonNull TasksDataSource tasksCacheDataSource) {
+        mTasksRemoteDataSource = tasksRemoteDataSource;
+        mTasksLocalDataSource = tasksLocalDataSource;
+        mTasksCacheDataSource = tasksCacheDataSource;
+    }
+
+    public static TasksRepository getInstance(@NonNull TasksDataSource tasksRemoteDataSource,
+                                              @NonNull TasksDataSource tasksLocalDataSource,
+                                              @NonNull TasksDataSource tasksCacheDataSource) {
+        if (INSTANCE == null) {
+            INSTANCE = new TasksRepository(tasksRemoteDataSource,
+                    tasksLocalDataSource, tasksCacheDataSource);
+        }
+        return INSTANCE;
+    }
 
     @Override
     public Flowable<List<Task>> getTasks() {
