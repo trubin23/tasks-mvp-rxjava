0da2eb0ef8f893209fe819b2b573b30b42eaeb20
Trubin Andrey <trubin23@gmail.com>
2018-09-20 23:41:51 +0500

    added TasksDetailActivity

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/taskdetail/TaskDetailActivity.java
@@ -0,0 +1,13 @@
+package ru.trubin23.tasks_mvp_rxjava.taskdetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+    }
+}
