c630e0ac85e2411cee2b5afe9e3d64ec1bab53c9
Trubin Andrey <trubin23@gmail.com>
2018-09-29 23:01:07 +0500

    add classes in AddEditTask package

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskContract.java
@@ -0,0 +1,4 @@
+package ru.trubin23.tasks_mvp_rxjava.addedittask;
+
+public interface AddEditTaskContract {
+}
---
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskPresenter.java
@@ -0,0 +1,4 @@
+package ru.trubin23.tasks_mvp_rxjava.addedittask;
+
+public class AddEditTaskPresenter {
+}
