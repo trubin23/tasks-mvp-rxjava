a2d89401d7f86d0e92222ab746b2d12590380b45
Trubin Andrey <trubin23@gmail.com>
2018-10-02 19:56:48 +0500

    implement constructor for TasksLocalRepository

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/source/local/TasksLocalRepository.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/source/local/TasksLocalRepository.java
@@ -1,16 +1,45 @@
 package ru.trubin23.tasks_mvp_rxjava.data.source.local;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.google.common.base.Optional;
+import com.squareup.sqlbrite2.BriteDatabase;
+import com.squareup.sqlbrite2.SqlBrite;
 
 import java.util.List;
 
 import io.reactivex.Flowable;
 import ru.trubin23.tasks_mvp_rxjava.data.Task;
+import ru.trubin23.tasks_mvp_rxjava.util.schedulers.BaseSchedulerProvider;
 
 public class TasksLocalRepository implements TasksLocalDataSource {
+
+    @Nullable
+    private static TasksLocalRepository INSTANCE;
+
+    @NonNull
+    private final BriteDatabase mBriteDatabase;
+
+    public static TasksLocalRepository getInstance(
+            @NonNull Context context,
+            @NonNull BaseSchedulerProvider schedulerProvider) {
+
+        if (INSTANCE == null) {
+            INSTANCE = new TasksLocalRepository(context, schedulerProvider);
+        }
+        return INSTANCE;
+    }
+
+    private TasksLocalRepository(@NonNull Context context,
+                                 @NonNull BaseSchedulerProvider schedulerProvider) {
+
+        TasksDbHelper dbHelper = new TasksDbHelper(context);
+        SqlBrite sqlBrite = new SqlBrite.Builder().build();
+        mBriteDatabase = sqlBrite.wrapDatabaseHelper(dbHelper, schedulerProvider.io());
+    }
 
     @Override
     public void setTasks(@NonNull List<Task> tasks) {
