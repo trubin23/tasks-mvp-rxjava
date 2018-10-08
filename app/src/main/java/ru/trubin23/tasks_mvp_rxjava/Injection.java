2c9ec3892f153090a84decc09bcd155a87445073
Trubin Andrey <trubin23@gmail.com>
2018-10-08 23:47:12 +0500

    finished implementing Injection class

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/Injection.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/Injection.java
@@ -4,16 +4,18 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import ru.trubin23.tasks_mvp_rxjava.data.source.TasksRepository;
+import ru.trubin23.tasks_mvp_rxjava.data.source.cache.TasksCacheRepository;
 import ru.trubin23.tasks_mvp_rxjava.data.source.local.TasksLocalRepository;
+import ru.trubin23.tasks_mvp_rxjava.data.source.remote.TasksRemoteRepository;
 import ru.trubin23.tasks_mvp_rxjava.util.schedulers.BaseSchedulerProvider;
 import ru.trubin23.tasks_mvp_rxjava.util.schedulers.SchedulerProvider;
 
 public class Injection {
 
-    public static TasksRepository provideTasksRepository(@NonNull Context context){
-        return TasksRepository.getInstance(null,
+    public static TasksRepository provideTasksRepository(@NonNull Context context) {
+        return TasksRepository.getInstance(TasksRemoteRepository.getInstance(),
                 TasksLocalRepository.getInstance(context, provideSchedulerProvider()),
-                null);
+                TasksCacheRepository.getInstance());
     }
 
     public static BaseSchedulerProvider provideSchedulerProvider() {
--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/source/remote/TasksRemoteRepository.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/source/remote/TasksRemoteRepository.java
@@ -1,4 +1,58 @@
 package ru.trubin23.tasks_mvp_rxjava.data.source.remote;
 
-public class TasksRemoteRepository {
+import android.support.annotation.NonNull;
+
+import com.google.common.base.Optional;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+import ru.trubin23.tasks_mvp_rxjava.data.Task;
+import ru.trubin23.tasks_mvp_rxjava.data.source.TasksDataSource;
+
+public class TasksRemoteRepository implements TasksDataSource {
+
+    private TasksRemoteRepository() {
+
+    }
+
+    public static TasksRemoteRepository getInstance() {
+        return new TasksRemoteRepository();
+    }
+
+
+    @Override
+    public Flowable<List<Task>> getTasks() {
+        return null;
+    }
+
+    @Override
+    public Flowable<Optional<Task>> getTask(@NonNull String taskId) {
+        return null;
+    }
+
+    @Override
+    public void saveTask(@NonNull Task task) {
+
+    }
+
+    @Override
+    public void updateTask(@NonNull Task task) {
+
+    }
+
+    @Override
+    public void deleteTask(@NonNull String taskId) {
+
+    }
+
+    @Override
+    public void completedTask(@NonNull String taskId, boolean completed) {
+
+    }
+
+    @Override
+    public void clearCompletedTask() {
+
+    }
 }
