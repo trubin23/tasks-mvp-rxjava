43fbe10737252cd2006f2a200917c6d7ed5d39be
Trubin Andrey <trubin23@gmail.com>
2018-10-10 23:52:55 +0500

    start to implement TaskDetailPresenter class

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/taskdetail/TaskDetailPresenter.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/taskdetail/TaskDetailPresenter.java
@@ -1,4 +1,44 @@
 package ru.trubin23.tasks_mvp_rxjava.taskdetail;
 
-public class TaskDetailPresenter {
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import ru.trubin23.tasks_mvp_rxjava.data.source.TasksRepository;
+import ru.trubin23.tasks_mvp_rxjava.util.schedulers.BaseSchedulerProvider;
+
+public class TaskDetailPresenter implements TaskDetailContract.Presenter {
+
+    @NonNull
+    private TasksRepository mTasksRepository;
+
+    @NonNull
+    private TaskDetailContract.View mTaskDetailView;
+
+    @NonNull
+    private BaseSchedulerProvider mSchedulerProvider;
+
+    @Nullable
+    private String mTaskId;
+
+    TaskDetailPresenter(@Nullable String taskId,
+                        @NonNull TasksRepository tasksRepository,
+                        @NonNull TaskDetailContract.View taskDetailView,
+                        @NonNull BaseSchedulerProvider schedulerProvider) {
+        mTaskId = taskId;
+        mTasksRepository = tasksRepository;
+        mTaskDetailView = taskDetailView;
+        mSchedulerProvider = schedulerProvider;
+
+        mTaskDetailView.setPresenter(this);
+    }
+
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
 }
