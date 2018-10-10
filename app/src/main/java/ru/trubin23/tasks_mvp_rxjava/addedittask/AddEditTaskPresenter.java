11d564fd0ed88993cbeac5a7adf33aba42be1681
Trubin Andrey <trubin23@gmail.com>
2018-10-10 23:56:08 +0500

    start to implement AddEditTaskPresenter class

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskPresenter.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskPresenter.java
@@ -1,4 +1,44 @@
 package ru.trubin23.tasks_mvp_rxjava.addedittask;
 
-public class AddEditTaskPresenter {
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import ru.trubin23.tasks_mvp_rxjava.data.source.TasksRepository;
+import ru.trubin23.tasks_mvp_rxjava.util.schedulers.BaseSchedulerProvider;
+
+public class AddEditTaskPresenter implements AddEditTaskContract.Presenter {
+
+    @NonNull
+    private TasksRepository mTasksRepository;
+
+    @NonNull
+    private AddEditTaskContract.View mAddEditTaskView;
+
+    @NonNull
+    private BaseSchedulerProvider mSchedulerProvider;
+
+    @Nullable
+    private String mTaskId;
+
+    AddEditTaskPresenter(@Nullable String taskId,
+                         @NonNull TasksRepository tasksRepository,
+                         @NonNull AddEditTaskContract.View addEditTaskView,
+                         @NonNull BaseSchedulerProvider schedulerProvider) {
+        mTaskId = taskId;
+        mTasksRepository = tasksRepository;
+        mAddEditTaskView = addEditTaskView;
+        mSchedulerProvider = schedulerProvider;
+
+        mAddEditTaskView.setPresenter(this);
+    }
+
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
 }
