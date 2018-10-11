82338145b525d4c07d1d916e15f3edc5cea25626
Trubin Andrey <trubin23@gmail.com>
2018-10-11 23:50:56 +0500

    start to implement TaskDetailFragment class

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/taskdetail/TaskDetailFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/taskdetail/TaskDetailFragment.java
@@ -1,9 +1,31 @@
 package ru.trubin23.tasks_mvp_rxjava.taskdetail;
 
-public class TaskDetailFragment implements TaskDetailContract.View {
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+public class TaskDetailFragment extends Fragment implements TaskDetailContract.View {
+
+    private TaskDetailContract.Presenter mPresenter;
+
+    @NonNull
+    public static TaskDetailFragment newInstance() {
+        return new TaskDetailFragment();
+    }
 
     @Override
     public void setPresenter(TaskDetailContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mPresenter.subscribe();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mPresenter.unsubscribe();
     }
 }
