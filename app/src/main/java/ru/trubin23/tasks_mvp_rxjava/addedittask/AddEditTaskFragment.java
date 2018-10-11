81f4b578422d6170e54b24f520170d272350cb4e
Trubin Andrey <trubin23@gmail.com>
2018-10-11 23:53:46 +0500

    start to implement AddEditTaskFragment class

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskFragment.java
@@ -1,9 +1,31 @@
 package ru.trubin23.tasks_mvp_rxjava.addedittask;
 
-public class AddEditTaskFragment implements AddEditTaskContract.View {
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+public class AddEditTaskFragment extends Fragment implements AddEditTaskContract.View {
+
+    private AddEditTaskContract.Presenter mPresenter;
+
+    @NonNull
+    public static AddEditTaskFragment newInstance() {
+        return new AddEditTaskFragment();
+    }
 
     @Override
     public void setPresenter(AddEditTaskContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mPresenter.subscribe();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mPresenter.unsubscribe();
     }
 }
