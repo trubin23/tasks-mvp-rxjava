272600e07ad9b8e80f6d3788942610b38bbe3561
Trubin Andrey <trubin23@gmail.com>
2018-10-12 23:59:07 +0500

    add StatisticsActivity class

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsActivity.java
@@ -0,0 +1,13 @@
+package ru.trubin23.tasks_mvp_rxjava.statistics;
+
+import android.os.Bundle;
+
+import ru.trubin23.tasks_mvp_rxjava.R;
+
+public class StatisticsActivityextends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
