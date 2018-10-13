a5d826794baf9081fa3d04cf248efa7e7436bde8
Trubin Andrey <trubin23@gmail.com>
2018-10-13 23:12:07 +0500

    continue implement Statistics package

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsContract.java
@@ -0,0 +1,15 @@
+package ru.trubin23.tasks_mvp_rxjava.statistics;
+
+import ru.trubin23.tasks_mvp_rxjava.BasePresenter;
+import ru.trubin23.tasks_mvp_rxjava.BaseView;
+
+public class StatisticsContract {
+
+    interface View extends BaseView<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
