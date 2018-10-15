84a9cf6c5e71958304fa3d96b390a456d742ea20
Trubin Andrey <trubin23@gmail.com>
2018-10-15 23:56:04 +0500

    continue to implement interaction in Statistics package

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsActivity.java
@@ -3,7 +3,9 @@ package ru.trubin23.tasks_mvp_rxjava.statistics;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import ru.trubin23.tasks_mvp_rxjava.Injection;
 import ru.trubin23.tasks_mvp_rxjava.R;
+import ru.trubin23.tasks_mvp_rxjava.util.ActivityUtils;
 
 public class StatisticsActivity extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class StatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics_act);
+
+        StatisticsFragment statisticsFragment =
+                (StatisticsFragment) getSupportFragmentManager().findFragmentById(R.id.content_frame);
+        if (statisticsFragment == null) {
+            statisticsFragment = new StatisticsFragment();
+            ActivityUtils.addFragmentToActivity(
+                    getSupportFragmentManager(), statisticsFragment, R.id.content_frame);
+        }
+
+        StatisticsPresenter tasksPresenter = new StatisticsPresenter(
+                Injection.provideTasksRepository(getApplicationContext()),
+                statisticsFragment,
+                Injection.provideSchedulerProvider());
     }
 }
--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsPresenter.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsPresenter.java
@@ -1,6 +1,30 @@
 package ru.trubin23.tasks_mvp_rxjava.statistics;
 
+import android.support.annotation.NonNull;
+
+import ru.trubin23.tasks_mvp_rxjava.data.source.TasksRepository;
+import ru.trubin23.tasks_mvp_rxjava.util.schedulers.BaseSchedulerProvider;
+
 public class StatisticsPresenter implements StatisticsContract.Presenter {
+
+    @NonNull
+    private TasksRepository mTasksRepository;
+
+    @NonNull
+    private StatisticsContract.View mTasksView;
+
+    @NonNull
+    private BaseSchedulerProvider mSchedulerProvider;
+
+    StatisticsPresenter(@NonNull TasksRepository tasksRepository,
+                        @NonNull StatisticsContract.View tasksView,
+                        @NonNull BaseSchedulerProvider schedulerProvider) {
+        mTasksRepository = tasksRepository;
+        mTasksView = tasksView;
+        mSchedulerProvider = schedulerProvider;
+
+        //mTasksView.setPresenter(this);
+    }
 
     @Override
     public void subscribe() {
