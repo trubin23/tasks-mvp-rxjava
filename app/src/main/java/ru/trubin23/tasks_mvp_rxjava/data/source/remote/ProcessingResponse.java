160cb9ae37788079773680761448fe30cdac721f
Trubin Andrey <trubin23@gmail.com>
2018-10-22 22:53:21 +0400

    add ProcessingResponse class in remote package

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/source/remote/ProcessingResponse.java
@@ -0,0 +1,35 @@
+package ru.trubin23.tasks_mvp_rxjava.data.source.remote;
+
+import android.support.annotation.NonNull;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ProcessingResponse<T> implements Callback<T> {
+
+    @Override
+    public void onResponse(Call<T> call, Response<T> response) {
+        if (response.isSuccessful()) {
+            T body = response.body();
+            if (body != null) {
+                responseBody(body);
+            } else {
+                dataNotAvailable();
+            }
+        } else {
+            dataNotAvailable();
+        }
+    }
+
+    @Override
+    public void onFailure(Call<T> call, Throwable t) {
+        dataNotAvailable();
+    }
+
+    public void responseBody(@NonNull T body) {
+    }
+
+    public void dataNotAvailable() {
+    }
+}
