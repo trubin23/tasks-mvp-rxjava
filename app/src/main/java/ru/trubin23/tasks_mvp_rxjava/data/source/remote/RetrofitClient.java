a2c7da2ad59bc1c8e1dab97203f5e52d42b6df68
Trubin Andrey <trubin23@gmail.com>
2018-10-23 23:58:11 +0400

    finish to implement RetrofitClient class

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/source/remote/RetrofitClient.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/data/source/remote/RetrofitClient.java
@@ -2,6 +2,9 @@ package ru.trubin23.tasks_mvp_rxjava.data.source.remote;
 
 import android.support.annotation.NonNull;
 
+import java.util.List;
+
+import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -23,40 +26,40 @@ class RetrofitClient {
         }
         return sRemoteService;
     }
-//
-//    static void getTasks(@NonNull Callback<List<NetworkTask>> callback) {
-//        RemoteService remoteService = getRemoteService();
-//        remoteService.getTasks().enqueue(callback);
-//    }
-//
-//    static void getTask(@NonNull String taskId, @NonNull Callback<NetworkTask> callback) {
-//        RemoteService remoteService = getRemoteService();
-//        remoteService.getTask(taskId).enqueue(callback);
-//    }
-//
-//    static void addTask(@NonNull NetworkTask task, @NonNull Callback<NetworkTask> callback) {
-//        RemoteService remoteService = getRemoteService();
-//        remoteService.addTask(task).enqueue(callback);
-//    }
-//
-//    static void updateTask(@NonNull NetworkTask task, @NonNull Callback<NetworkTask> callback) {
-//        RemoteService remoteService = getRemoteService();
-//        remoteService.updateTask(task.getId(), task).enqueue(callback);
-//    }
-//
-//    static void completeTask(@NonNull String taskId, @NonNull StatusOfTask statusOfTask,
-//                             @NonNull Callback<NetworkTask> callback){
-//        RemoteService remoteService = getRemoteService();
-//        remoteService.completeTask(taskId, statusOfTask).enqueue(callback);
-//    }
-//
-//    static void deleteTask(@NonNull String taskId, @NonNull Callback<NetworkTask> callback) {
-//        RemoteService remoteService = getRemoteService();
-//        remoteService.deleteTask(taskId).enqueue(callback);
-//    }
-//
-//    static void deleteCompletedTasks(@NonNull Callback<Integer> callback){
-//        RemoteService remoteService = getRemoteService();
-//        remoteService.deleteCompletedTasks().enqueue(callback);
-//    }
+
+    static void getTasks(@NonNull Callback<List<NetworkTask>> callback) {
+        RemoteService remoteService = getRemoteService();
+        remoteService.getTasks().enqueue(callback);
+    }
+
+    static void getTask(@NonNull String taskId, @NonNull Callback<NetworkTask> callback) {
+        RemoteService remoteService = getRemoteService();
+        remoteService.getTask(taskId).enqueue(callback);
+    }
+
+    static void addTask(@NonNull NetworkTask task, @NonNull Callback<NetworkTask> callback) {
+        RemoteService remoteService = getRemoteService();
+        remoteService.addTask(task).enqueue(callback);
+    }
+
+    static void updateTask(@NonNull NetworkTask task, @NonNull Callback<NetworkTask> callback) {
+        RemoteService remoteService = getRemoteService();
+        remoteService.updateTask(task.getId(), task).enqueue(callback);
+    }
+
+    static void completeTask(@NonNull String taskId, @NonNull StatusOfTask statusOfTask,
+                             @NonNull Callback<NetworkTask> callback){
+        RemoteService remoteService = getRemoteService();
+        remoteService.completeTask(taskId, statusOfTask).enqueue(callback);
+    }
+
+    static void deleteTask(@NonNull String taskId, @NonNull Callback<NetworkTask> callback) {
+        RemoteService remoteService = getRemoteService();
+        remoteService.deleteTask(taskId).enqueue(callback);
+    }
+
+    static void deleteCompletedTasks(@NonNull Callback<Integer> callback){
+        RemoteService remoteService = getRemoteService();
+        remoteService.deleteCompletedTasks().enqueue(callback);
+    }
 }
