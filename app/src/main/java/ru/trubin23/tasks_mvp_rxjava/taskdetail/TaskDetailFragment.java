24717304167025668241a586a9b5a2569614b4d6
Trubin Andrey <trubin23@gmail.com>
2018-10-25 23:57:28 +0400

    start bind view from TaskDetailFragment with implementation

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/taskdetail/TaskDetailFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/taskdetail/TaskDetailFragment.java
@@ -1,7 +1,15 @@
 package ru.trubin23.tasks_mvp_rxjava.taskdetail;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.trubin23.tasks_mvp_rxjava.R;
 
 public class TaskDetailFragment extends Fragment implements TaskDetailContract.View {
 
@@ -16,6 +24,20 @@ public class TaskDetailFragment extends Fragment implements TaskDetailContract.V
     public void setPresenter(TaskDetailContract.Presenter presenter) {
         mPresenter = presenter;
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.taskdetail_frag, container, false);
+
+        setHasOptionsMenu(true);
+
+        //getActivity().findViewById((R.id.fab_edit_task)
+        //        .setOnClickListener{() -> mPresenter.editTask() }
+
+        return root;
+    }
+
 
     @Override
     public void onResume() {
