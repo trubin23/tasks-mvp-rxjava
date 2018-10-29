b5c0283c56af18c8b436470cd2cfee7a5921fc2a
Trubin Andrey <trubin23@gmail.com>
2018-10-29 23:52:13 +0500

    add toolbar to AddEditTaskActivity

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskActivity.java
@@ -1,7 +1,9 @@
 package ru.trubin23.tasks_mvp_rxjava.addedittask;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import ru.trubin23.tasks_mvp_rxjava.Injection;
 import ru.trubin23.tasks_mvp_rxjava.R;
@@ -13,6 +15,14 @@ public class AddEditTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_edit_task_act);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
 
         AddEditTaskFragment addEditTaskFragment = (AddEditTaskFragment)
                 getSupportFragmentManager().findFragmentById(R.id.content_frame);
