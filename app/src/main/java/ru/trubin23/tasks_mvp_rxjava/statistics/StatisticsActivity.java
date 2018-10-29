d5512b90fd538807dfcfac6fc9895509cd00a642
Trubin Andrey <trubin23@gmail.com>
2018-10-29 23:39:03 +0500

    add toolbar to StatisticsActivity

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsActivity.java
@@ -1,7 +1,9 @@
 package ru.trubin23.tasks_mvp_rxjava.statistics;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import ru.trubin23.tasks_mvp_rxjava.Injection;
 import ru.trubin23.tasks_mvp_rxjava.R;
@@ -13,6 +15,14 @@ public class StatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics_act);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         StatisticsFragment statisticsFragment =
                 (StatisticsFragment) getSupportFragmentManager().findFragmentById(R.id.content_frame);
