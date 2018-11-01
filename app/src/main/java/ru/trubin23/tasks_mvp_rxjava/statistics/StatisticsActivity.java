aadd0a0f2a4f975de288e8d9d4f6b6b556b08769
Trubin Andrey <trubin23@gmail.com>
2018-11-01 23:33:15 +0500

    add setupNavigationDrawer function in StatisticsActivity

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/statistics/StatisticsActivity.java
@@ -1,6 +1,8 @@
 package ru.trubin23.tasks_mvp_rxjava.statistics;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.NavUtils;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -20,6 +22,8 @@ public class StatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics_act);
+
+        setupNavigationDrawer();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -51,5 +55,28 @@ public class StatisticsActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupNavigationDrawer() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if (navigationView == null) {
+            return;
+        }
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.list_nav_menu_item:
+                    NavUtils.navigateUpFromSameTask(StatisticsActivity.this);
+                    break;
+                case R.id.statistics_nav_menu_item:
+                    break;
+            }
+
+            item.setChecked(true);
+            mDrawerLayout.closeDrawers();
+            return true;
+        });
     }
 }
