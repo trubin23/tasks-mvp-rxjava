516753605014f44252ca5ba162e05326971ce8a9
Trubin Andrey <trubin23@gmail.com>
2018-11-02 23:15:25 +0500

    add implementation for saveTask function in AddEditTaskPresenter

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskPresenter.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskPresenter.java
@@ -3,6 +3,7 @@ package ru.trubin23.tasks_mvp_rxjava.addedittask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import ru.trubin23.tasks_mvp_rxjava.data.Task;
 import ru.trubin23.tasks_mvp_rxjava.data.source.TasksRepository;
 import ru.trubin23.tasks_mvp_rxjava.util.schedulers.BaseSchedulerProvider;
 
@@ -44,6 +45,26 @@ public class AddEditTaskPresenter implements AddEditTaskContract.Presenter {
 
     @Override
     public void saveTask(String title, String description) {
+        Task task;
+        if (isNewTask()) {
+            task = new Task(title, description);
+        } else {
+            task = new Task(title, description, mTaskId);
+        }
 
+        if (task.isEmpty()) {
+            if (mAddEditTaskView != null) {
+                mAddEditTaskView.showEmptyTaskError();
+            }
+        } else {
+            if (isNewTask()) {
+                mTasksRepository.saveTask(task);
+            } else {
+                mTasksRepository.updateTask(task);
+            }
+            if (mAddEditTaskView != null) {
+                mAddEditTaskView.showTaskList();
+            }
+        }
     }
 }
