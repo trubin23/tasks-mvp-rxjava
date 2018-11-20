f4c96c772597cf41935040f5eea437fffc057385
Trubin Andrey <trubin23@gmail.com>
2018-11-20 23:58:30 +0500

    add showMessage function in TasksFragment class

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/tasks/TasksFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/tasks/TasksFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -191,12 +192,19 @@ public class TasksFragment extends Fragment implements TasksContract.View {
 
     @Override
     public void showTaskMarkedComplete() {
-
+        showMessage(getString(R.string.task_marked_complete));
     }
 
     @Override
     public void showTaskMarkedActive() {
+        showMessage(getString(R.string.task_marked_active));
+    }
 
+    private void showMessage(String message) {
+        View view = getView();
+        if (view != null) {
+            Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+        }
     }
 
     @Override
