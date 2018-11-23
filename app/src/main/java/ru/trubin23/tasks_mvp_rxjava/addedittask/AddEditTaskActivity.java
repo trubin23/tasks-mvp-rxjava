798e6c226f2e921374fb829cb9b7ea1f704b6f72
Trubin Andrey <trubin23@gmail.com>
2018-11-23 21:49:28 +0500

    add setToolbarTitle function in AddEditTaskActivity

--- app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_rxjava/addedittask/AddEditTaskActivity.java
@@ -1,6 +1,7 @@
 package ru.trubin23.tasks_mvp_rxjava.addedittask;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,6 +16,10 @@ public class AddEditTaskActivity extends AppCompatActivity {
 
     public static final String EXTRA_TASK_ID = "EXTRA_TASK_ID";
 
+    private AddEditTaskPresenter mAddEditTaskPresenter;
+
+    private ActionBar mActionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +27,10 @@ public class AddEditTaskActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowHomeEnabled(true);
+        mActionBar = getSupportActionBar();
+        if (mActionBar != null) {
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+            mActionBar.setDisplayShowHomeEnabled(true);
         }
 
         AddEditTaskFragment addEditTaskFragment = (AddEditTaskFragment)
@@ -36,10 +41,18 @@ public class AddEditTaskActivity extends AppCompatActivity {
                     getSupportFragmentManager(), addEditTaskFragment, R.id.content_frame);
         }
 
-        AddEditTaskPresenter addEditTaskPresenter = new AddEditTaskPresenter(
+        mAddEditTaskPresenter = new AddEditTaskPresenter(
                 null,
                 Injection.provideTasksRepository(getApplicationContext()),
                 addEditTaskFragment,
                 Injection.provideSchedulerProvider());
+    }
+
+    private void setToolbarTitle(@Nullable String taskId){
+        if (taskId == null) {
+            mActionBar.setTitle(R.string.add_task);
+        } else {
+            mActionBar.setTitle(R.string.edit_task);
+        }
     }
 }
